2f8dc97aff74373d288be53e1a9c72178aa85f36
Ajay <mailtoajay7007@gmail.com>
2025-08-24 16:04:10 +0530

    formatt

--- background.js
+++ background.js
@@ -1,24 +1,18 @@
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (message.action === "save_highlight") {
+    chrome.storage.local.get(["highlights"], (r) => {
+      const highlights = r.highlights || [];
+      highlights.push(message.text);
 
-  if (message.action === 'save_highlight') {
-    chrome.storage.local.get(['highlights'],(r)=>{
-        const highlights = r.highlights || [];
-        highlights.push(message.text);
-
-        chrome.storage.local.set({highlights:highlights}, ()=>{
-            console.log("saved!",highlights);
-            
-        })
-
-    })
-
-  }else if (message.action === 'delete_highlight') {
-    chrome.storage.local.get(['highlights'],(r)=>{
-        const highlights = r.highlights || [];
-        highlights.splice(message.index, 1);
+      chrome.storage.local.set({ highlights: highlights }, () => {
+        console.log("saved!", highlights);
+      });
+    });
+  } else if (message.action === "delete_highlight") {
+    chrome.storage.local.get(["highlights"], (r) => {
+      const highlights = r.highlights || [];
+      highlights.splice(message.index, 1);
       chrome.storage.local.set({ highlights: highlights });
-
-    })
-
+    });
   }
 });
--- popup.js
+++ popup.js
@@ -4,30 +4,28 @@ document.addEventListener("DOMContentLoaded", () => {
     const container = document.getElementById("highlights-container");
     container.innerHTML = "";
     if (highlights.length > 0) {
-      
-      highlights.forEach((text,index) => {
+      highlights.forEach((text, index) => {
         const highlightElement = document.createElement("div");
         highlightElement.textContent = text;
         const textElement = document.createElement("span");
         textElement.textContent = text;
-        
-        
+
         const deleteButton = document.createElement("button");
         deleteButton.textContent = "Delete";
         highlightElement.appendChild(textElement);
         highlightElement.appendChild(deleteButton);
 
-
-        deleteButton.addEventListener('click',()=>{
-             chrome.runtime.sendMessage({ action: 'delete_highlight', index: index });
-             highlightElement.remove();
-        })
-
+        deleteButton.addEventListener("click", () => {
+          chrome.runtime.sendMessage({
+            action: "delete_highlight",
+            index: index,
+          });
+          highlightElement.remove();
+        });
 
         container.appendChild(highlightElement);
       });
     } else {
-      
       container.innerHTML = "<p>No highlights saved yet!</p>";
     }
   });
